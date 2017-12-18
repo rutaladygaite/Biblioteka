8d6036a61ff48c2dffd1a27bdd3ea5291c1d0a2c
Ruta Ladygaite <34306693+rutaladygaite@users.noreply.github.com>
2017-12-18 15:11:45 +0200

    Add files via upload

--- Biblioteka.js
+++ Biblioteka.js
@@ -5,7 +5,14 @@ var dateElement = document.querySelector("#date");
 var descriptionElement = document.querySelector("#description");
 
 //cia saugosime visas ivestas knygas
-var booklist = [];
+var bookList = [];
+var booksJSON = localStorage.getItem("books");
+var bookList = JSON.parse(booksJSON);
+
+if (bookList === null){
+	bookList = [];
+}
+
 var addBookButton = document.querySelector(".btn-save-book")
 
 addBookButton.addEventListener("click", addBook);
@@ -23,16 +30,27 @@ function addBook() {
 	}
 
 	var record = {
-		author:author,
-		title: title,
-		publisher: publisher,
-		date:date,
-		description:description,
+		"author":author,
+		"title": title,
+		"publisher": publisher,
+		"date":date,
+		"description":description,
 	}
 
-booklist.push(record);
+if (checkForDublicates(record)){ //kreipiames i funkcija pries irasydami
+	alert ("Tokia knyga yra jau irasyta");
+	return;
+
+}
+
+bookList.push(record); //issaugoti
+booksJSON = JSON.stringify(bookList); //perrasyti JSON formatu
+localStorage.setItem("books", booksJSON); //dar karta issaugoti
+
 renderBooks();
 clearForm();
+
+document.getElementById('screen').style.display = 'none'
 }
 
 var clearButton = document.querySelector(".btn-clear-form");
@@ -50,17 +68,32 @@ function clearForm(){
 
 var formElement = document.querySelector(".library-form");
 
+function checkForDublicates(record){
+	var isDuplicate = false;
+
+	bookList.forEach(function(item){ //kiekvienam taikysim funkcija, kurios vardas item
+		if(item.author == record.author && item.title == record.title && item.date == record.date){
+			isDuplicate = true;
+
+		}
+	});
+	return isDuplicate;
+}
+
+
 function renderBooks(){
-	if (!booklist) return;
+	if (!bookList) return;
 	var result = "";
-	for (var book of booklist) {
+	for (var book of bookList) {
 		result += `
 		<div class = "book clearfix">
+		<span onclick = "openModal(this.id)" style = "cursor:pointer;" id= "${book.title}">
 		<h2>${book.author}</h2>
 		<h4>${book.title}</h4>
 		<p>Isleido: ${book.publisher}</p>
 		<date>${book.date}</date>
 		<p>${book.description}</p>
+		</span>
 		</div>
 		<hr>
 
@@ -69,3 +102,38 @@ function renderBooks(){
 	var bookListElement = document.querySelector(".book-list");
 	bookListElement.innerHTML = result;
 }
+
+function openModal(clicked_id){ //turime sugeneruoti koda, kad kievkienam ivedimui butu priskiriamas id
+	var book = findElement(clicked_id);
+	document.getElementById('screen-item').style.display = 'block';
+	var result = `
+	<div class  = "modal-screen" onclick="event.stopPropagation();">
+		<header class = "popup-container">
+		<h1> ${book.author} ${book.title} </h1>
+		</header>
+		<hr>
+		<div class = "popup-container">
+			<h4> Leidykla: "${book.publisher}" </h4>
+			<h4> Data: "${book.date}" </h4>
+			<p> ${book.description} </p>
+	</div>
+	<footer class = "popup-container">
+	<button type = "button" class = "btn-primary" onclick = "document.getElementById('screen-item').style.display = 'none'"> Uzdaryti </button>
+	
+</footer>
+	`
+	document.getElementById("screen-item").innerHTML =result;
+}
+
+function findElement (bookName) {
+	var count = 0;
+	for (book of bookList){
+		if(book.title == bookName){
+			return book;
+		}
+		count++;
+	}
+}
+
+renderBooks();
+
